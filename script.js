fb0aefc2e328fdab35ee1bc8ccc5f97dee0405cf
RgerWeb <mckeenweb@gmail.com>
2023-09-03 21:00:24 +0800

    gg

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ function myFunction() {
 	document.getElementById("answer").innerHTML="Your input is blank";      
 	} //else if([(inputText === ""),inputText.toLowerCase().match(/^me$/),(inputText === "")]) {
 	//document.getElementById("answer").innerHTML="We All Gonna Die!";} 
-	else if(inputText.toLowerCase().includes(" my ")) {
-	document.getElementById("answer").innerHTML="We All Gonna Die!";
-	} else if(inputText === " me ") {
+	//else if(inputText.toLowerCase().includes(" my ")) {
+	//document.getElementById("answer").innerHTML="We All Gonna Die!";} 
+	else if(inputText === " me "||" my ") {
 	document.getElementById("answer").innerHTML="We All Gonna Die! Congratulations! You hit the sweetspot of humanity, the word *me* ";
-
-
 	} else {
 	document.getElementById("answer").innerHTML="Who cares?";      
 	}
