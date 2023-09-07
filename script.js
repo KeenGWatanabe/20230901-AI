cf941ee755ec90d25518dc0ac5f73dffe79e1f94
RgerWeb <mckeenweb@gmail.com>
2023-09-07 20:55:27 +0800

    gg

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function myFunction() {
 		} else if(inputText.toLowerCase().indexOf("asshole")>-1){
 	document.getElementById("answer").innerHTML='<a href="https://sinkapoor.blogspot.com/2023/09/irritants-of-society.html">click for blogging on assholes</a>';       
 		} else if(inputText.toLowerCase().indexOf("god")>-1){
-	document.getElementById("answer").innerHTML='<a href="https://www.theguardian.com/education/2004/mar/08/highereducation.uk1#:~:text=A%20scientist%20has%20calculated%20that,existence%20of%20an%20omnipotent%20being.">A scientist has calculated that there is a 67% chance that God exists</a>';       
+	document.getElementById("answer").innerHTML='<a href="https://www.theguardian.com/education/2004/mar/08/highereducation.uk1#:~:text=A%20scientist%20has%20calculated%20thatexistence%20of%20an%20omnipotent%20being.">A scientist has calculated that there is a 67% chance that God exists</a>';       
 		} else {
 	document.getElementById("answer").innerHTML="Who cares?";      
 		}
