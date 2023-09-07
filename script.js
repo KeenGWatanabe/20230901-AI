64f669114cc257232597ef292e9b5799960d42da
KeenGWatanabe <mckeenweb@gmail.com>
2023-09-08 06:36:00 +0800

    target blank

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ function myFunction() {
 	document.getElementById("answer").innerHTML=`Congratulations! WE ALL GONNA DIE! <br> You hit the sweetspot of humanity, the word "I" `;
 		}
 	else if(inputText.toLowerCase().indexOf("asshole")>-1){
-		document.getElementById("answer").innerHTML='<a href="https://sinkapoor.blogspot.com/2023/09/irritants-of-society.html">click for blogging on assholes</a>';       
+		document.getElementById("answer").innerHTML='<a href="https://sinkapoor.blogspot.com/2023/09/irritants-of-society.html"  target="_blank">click for blogging on assholes</a>';       
 	} 
 	else if(inputText.toLowerCase().indexOf("god")>-1){
-		document.getElementById("answer").innerHTML='<a href="https://www.theguardian.com/education/2004/mar/08/highereducation.uk1#:~:text=A%20scientist%20has%20calculated%20thatexistence%20of%20an%20omnipotent%20being.">A scientist has calculated that there is a 67% chance that God exists</a>'; 
+		document.getElementById("answer").innerHTML='<a href="https://www.theguardian.com/education/2004/mar/08/highereducation.uk1#:~:text=A%20scientist%20has%20calculated%20thatexistence%20of%20an%20omnipotent%20being."  target="_blank">A scientist has calculated that there is a 67% chance that God exists</a>'; 
 	}
 	else if(inputText.toLowerCase().indexOf("the developer")>-1){
-		document.getElementById("answer").innerHTML='<a href="http://web.mckeen.sg">developer</a>'; 
+		document.getElementById("answer").innerHTML='<a href="http://web.mckeen.sg" target="_blank">developer</a>'; 
 	}
 	else if(inputText.toLowerCase().indexOf("the weather")>-1){
-		document.getElementById("answer").innerHTML='<a href="http://www.weather.gov.sg/lightning/lightning/lightningalertinformationsystem.jsp">click for SG weather</a>'; 
+		document.getElementById("answer").innerHTML='<a href="http://www.weather.gov.sg/lightning/lightning/lightningalertinformationsystem.jsp"  target="_blank">click for SG weather</a>'; 
 	}
 	else if(inputText.toLowerCase().indexOf("xx")>-1){
 		document.getElementById("answer").innerHTML='<a href="">mt</a>'; 
