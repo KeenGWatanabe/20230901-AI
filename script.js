4144bef5e632f1f928b06c7da32291dee06b8132
RgerWeb <mckeenweb@gmail.com>
2023-09-12 06:13:46 +0800

    remove mck web

--- script.js
+++ script.js
@@ -20,9 +20,6 @@ function myFunction() {
 	else if(inputText.toLowerCase().indexOf("god")>-1){
 		document.getElementById("answer").innerHTML='<a href="https://www.theguardian.com/education/2004/mar/08/highereducation.uk1#:~:text=A%20scientist%20has%20calculated%20thatexistence%20of%20an%20omnipotent%20being."  target="_blank">A scientist has calculated that there is a 67% chance that God exists</a>'; 
 		}
-	else if(inputText.toLowerCase().indexOf("the developer")>-1){
-		document.getElementById("answer").innerHTML='<a href="http://web.mckeen.sg" target="_blank">developer</a>'; 
-		}
 	else if(inputText.toLowerCase().indexOf("singapore weather")>-1){
 		document.getElementById("answer").innerHTML='<a href="http://www.weather.gov.sg/lightning/lightning/lightningalertinformationsystem.jsp"  target="_blank">click for SG weather</a>'; 
 		}
